17ff81ddc9605864893d2c849e5f46f277650540
shanicesauce <shanicesauceda@gmail.com>
2022-10-07 23:35:59 -0500

    created user table data

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+//import sequelize constructor from library
+const Sequelize = require('sequelize');
+
+require('dotenv').config();
+
+//connection to db , pass in sql
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306
+});
+
+module.exports = sequelize;
---
+++ models/User.js
@@ -0,0 +1,63 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+//user model
+class User extends Model {}
+
+//define table columns and configuration
+
+User.init(
+    {
+        //define an id column
+        id: {
+            //use the special sequelize datatypes object provide what type of data it is
+            type: DataTypes.INTEGER,
+            // same as NOT NULL in sql
+            allowNull: false,
+            //primary key
+            primaryKey: true,
+            //auto increment
+            autoIncrement: true
+        },
+        //username column 
+        username: {
+            type: DataTypes.STRING,
+            allowNull:false
+        },
+        //email
+        email: {
+            type:DataTypes.STRING,
+            allowNull:false,
+            //no duplicate email values
+            unique:true,
+            // if allowNull is set to false, we can run our data through validators before creating the table data
+            validate: {
+                isEmail: true
+            }
+        },
+        //password
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                //password must be at least 4 characters long
+                len: [4]
+            }
+        }
+    },
+    {
+        //table configuration options (https://sequelize.org/v5/manual/models-definition.html#configuration))
+        //imported sequelize connection (to direct connection to db)
+        sequelize,
+        //don't auto create createdAt/updatedAt timestamps
+        timestamps:false,
+        //don't pluralize the name of db
+        freezeTableName: true,
+        //use underscores instead of camel case
+        underscored:true,
+        //make model name lowercase in db
+        modelName:'user'
+    }
+);
+
+module.exports = User;

837fc4541852d76574e0fa9424eedb304354e256
shanicesauce <shanicesauceda@gmail.com>
2022-10-09 23:29:23 -0500

    hashed password

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -49,6 +49,28 @@ router.post('/', (req,res) => {
     });
 });
 
+//POST /api/users/login
+router.post('/login', (req,res) => {
+    //expects {email:'name@email.com, password: 'password1}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if(!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address' });
+            return;
+        }
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if(!validPassword){
+            res.status(400).json({ message: 'Incorrect password!'});
+            return;
+        }
+        
+        res.json({ user: dbUserData, message: 'You are now logged in!' });
+    });
+});
+
 //PUT /api/user/1
 router.put('/:id', (req,res) => {
     //expects {username: 'name', email:'name@email.com, password: 'password1}
